5b53b1b9ff101473249b635e11f983672a43af8b
SafonovaNatalya <116429612+SafonovaNatalya@users.noreply.github.com>
2023-04-07 14:27:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,47 +21,44 @@ int getPrior(char ch) {
 
 std::string infx2pstfx(std::string inf) {
   TStack<char, 100> stack1;
-  int helper = -1;
   std::string postvir = "";
   for (int i = 0; i < strlen(inf); i++) {
-  if ((inf[i] = '0') || (inf[i] = '1') || (inf[i] = '2') || (inf[i] = '3') ||
-     (inf[i] = '4') || (inf[i] = '5') || (inf[i] = '6') || (inf[i] = '7') ||
-     (inf[i] = '8') || (inf[i] = '9'))
-  postvir = postvir + inf[i] + " ";
-  else {
-  if (inf[i] == '(') {
-    stack1.push(inf[i]);
-    helper += 1;
-  } else {
-    if ((inf[i] == '+') || (inf[i] == '-') ||
-        (inf[i] == '*') || (inf[i] == '/')) {
-     if (getPrior(inf[i]) > getPrior(stack1[helper]) || (isEmpty())) {
-      stack1.push(inf[i]);
-      helper += 1;
-     } else {
-      postvir = postvir + stack1.pop(stack[helper]) + " ";
-      stack1.push(inf[i]);
+    int prior = getPrior (inf[i]);
+    if (prior == -1) {
+      postvir += inf[i];
+    } else {
+      if (stack1.get() < prior || prior == 0 || stack1.isEmpty()) {
+        stack1.push(inf[i]);
+      } else if (inf[i] == ')') {
+        char helper = stack1.get();
+        while (getPrior(helper) >= prior) {
+          postvir += helper;
+          stack1.pop();
+          helper = stack1.get();
+        }
+        stack1.pop();
+      } else {
+        char helper = stack1.get();
+        while (getPrior(helper) >= prior) {
+          postvir += helper;
+          stack1.pop();
+          helper = stack1.get();
+        }
+        stack1.push(inf[i]);
+      }
     }
-  } else if (inf[i] == ')') {
-     while (stack1[helper] != '(') {
-      postvir = postvir + stack1.pop(stack[helper]) + " ";
-      helper--;
-     }
-     if (stack1[helper] == '(') {
-      stack1.pop(stack[helper]);
-      helper--;
-     }
-    }
-   }
   }
-  if (i == (strlen(inf) - 1)) {
-     while (helper > -1) {
-      postvir = postvir + stack1.pop(stack[helper]) + " ";
-      helper--;
-     }
-   }
-}
-  return std::string(postvir);
+  while (!stack1.isEmpty()) {
+        postvir += stack1.get();
+        stack1.pop();
+    }
+  if (2 >= postvir.length()) return postvir;
+    int k = 2 - postvir.length() % 2;
+    std::string otvet(postvir, 0, k);
+    for (auto it = postvir.begin() + k; it != postvir.end();) {
+        otvet += ' '; otvet += *it++;;
+    }
+    return otvet;
 }
 
 int counting(int i, int j, char ch) {
@@ -75,32 +72,29 @@ int counting(int i, int j, char ch) {
   case '/':
   return i/j;
   }
-  return -1;
+  return 0;
 }
-
+  
 int eval(std::string pref) {
   TStack<int, 100> stack2;
-  int size = -1;
-  int result = 0;
-  int j = 0;
-  std::string helper = pref;
-  while (j < strlen(helper)) {
-  if ((helper[i] = '0') || (helper[i] = '1') ||
-      (helper[i] = '2') || (helper[i] = '3') ||
-     (helper[i] = '4') || (helper[i] = '5') ||
-      (helper[i] = '6') || (helper[i] = '7') ||
-     (helper[i] = '8') || (helper[i] = '9')) {
-  stack2.push(helper.substr(0, helper->IndexOf(" "));
-  size += 1;
-  helper = helper.substr((helper->IndexOf(" ")) + 1, (strlen(helper) - 1));
-  } else if ((helper[i] = '+') || (helper[i] = '-') || (helper[i] = '*')
-             || (helper[i] = '/')) {
-  stack2.push(counting(stack2.pop(stack2[size - 1]),
-                       stack2.pop(stack2[size]), helper[i]);
-  helper = helper.substr((helper->IndexOf(" ")) + 1, (strlen(helper) - 1));
-  size--;
-              }
+  std::string helper = "";
+  for (int i = 0; i < pref.size(); i++) {
+    if (getPrior(pref[i]) == -1) {
+      if (pref[i] == ' ') {
+        continue;
+      } else if (isdigit(post[i+1])) {
+        helper += pref[i];
+        continue;
+      } else {
+        helper += pref[i];
+        stack2.push (atoi(helper.c_str()));
+        helper = "";
+      }
+    } else {
+      int j = stack2.pop();
+      int k = stack2.pop();
+      stack2.push(counting(j, k, pref[i]));
+    }
   }
-  }
-  return stack[size];
+  return stack2.get();
 }
